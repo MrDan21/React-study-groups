1a46e14348ee5dcafbf19010da04f88add63cfd1
Nino_God <95883939+TsNekroz@users.noreply.github.com>
2024-07-15 11:58:38 -0700

    Agrega funcionalidad de eliminar grupos

--- src/pages/groups/Index.js
+++ src/pages/groups/Index.js
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Swal from 'sweetalert2'; 
 import { useNavigate } from "react-router-dom";
+
 
 export const GroupsIndex = () => {
   const [groups, setGroups] = useState([
     {
+      id: 1,
       name: "Ingles",
       description:
         "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo",
       category: "Idiomas",
     },
     {
+      id: 2,
       name: "Ingles II",
       description:
         "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo",
@@ -17,6 +22,34 @@ export const GroupsIndex = () => {
     },
   ]);
 
+  const handleDeleteGroup = (id) => {
+    axios.delete(`http://localhost:8000/api/groups/${id}/`)
+      .then((response) => {
+        if (response.status === 204) { 
+          const updatedGroups = groups.filter((group) => group.id !== id);
+          setGroups(updatedGroups);
+          Swal.fire({
+            title: "Deleted!",
+            text: "The group has been deleted.",
+            icon: "success"
+          });
+        } else {
+          Swal.fire({
+            title: "Error!",
+            text: "Failed to delete the group.",
+            icon: "error"
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting group:", error);
+        Swal.fire({
+          title: "Error!",
+          text: "Failed to delete the group. Please try again later.",
+          icon: "error"
+        });
+      });
+  };
   const navigate = useNavigate();
   
   const handleNewGroupClick = () => {
@@ -34,36 +67,30 @@ export const GroupsIndex = () => {
         </button>
       </div>
 
-      <section class="text-gray-600 body-font">
-        <div class="container p-24 mx-auto">
-          <div class="flex flex-wrap -m-4">
+
+      <section className="text-gray-600 body-font">
+        <div className="container p-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
             {groups.map((group) => (
-              <div class="p-4 lg:w-1/3">
-                <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{group.category}</h2>
-                  <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">{group.name}</h1>
-                  <p class="leading-relaxed mb-3">DESCRIPTION</p>
-                  <a class="text-indigo-500 inline-flex items-center">
-                    SHOW
-                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                  <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-                    <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                        <circle cx="12" cy="12" r="3"></circle>
+              <div key={group.id} className="p-4 lg:w-1/3">
+                <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                  <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{group.category}</h2>
+                  <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">{group.name}</h1>
+                  <p className="leading-relaxed mb-3">{group.description}</p>
+                  <div className="flex justify-center">
+                    <button
+                      onClick={() => handleDeleteGroup(group.id)}
+                      className="text-red-500 inline-flex items-center mr-4 "
+                    >
+                      DELETE
+                    </button>
+                    <a className="text-indigo-500 inline-flex items-center mr-4">
+                      SHOW
+                      <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
                       </svg>
-                      1.2K
-                    </span>
-                    <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                      </svg>
-                      6
-                    </span>
+                    </a>
                   </div>
                 </div>
               </div>
